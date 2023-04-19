253a9293ed23748201683c8a2f01a9d7de9f867b
Emanuel <emanunelzsmj@gmail.com>
2023-04-19 01:48:08 -0300

    Expressoes regulares

---
+++ expressoes-regulares/index.js
@@ -0,0 +1,8 @@
+function PhoneNumber(phoneNumberString){
+    const fixedString = phoneNumberString.replace(/[\sa-zA-Z]/g, "")
+    this.countryCode = fixedString.match(/(?<=\+)\d{1,3}/)[0]
+    this.ddd = fixedString.match(/(?<=\()\d+(?=\))/)[0]
+    this.number = fixedString.match(/(?<=\)).+/)[0].replace(/-/g,"")
+}
+
+console.log(new PhoneNumber("+55(85)99786-9621"))
